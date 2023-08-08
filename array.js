109ae44e613aec270961151ab4b59e744ffd380b
saad200224 <saad19021994@outlook.com>
2023-08-08 09:14:59 +0600

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// console.log(Array.isArray(arr));
+
+
+// console.log(arr.includes(30));
+
+const arr2 = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+
+// console.log(arr.concat(arr2));
+
+// console.log(arr.slice(3,6));
+
+// console.log(arr.splice(3, 3));
+
+console.log(arr.splice(2, 3, 88, 98));
+
+console.log(arr);
+
